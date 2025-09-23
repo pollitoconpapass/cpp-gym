564b44c0bb953f47e767a412d76b1fb85e0cd080
pollitoconpapass <jalbertoqz@gmail.com>
2025-09-22 23:20:17 -0500

    [ Added string-repeat kata instructions ]

--- string-repeat/main.cpp
+++ string-repeat/main.cpp
@@ -1,3 +1,13 @@
+/*
+Write a function that accepts a non-negative integer n and a string s as parameters, and returns a string of s repeated exactly n times.
+
+Examples (input -> output)
+6, "I"     -> "IIIIII"
+5, "Hello" -> "HelloHelloHelloHelloHello"
+
+LINK: https://www.codewars.com/kata/57a0e5c372292dd76d000d7e/cpp
+*/
+
 #include <iostream>
 #include <string>
 using namespace std;
